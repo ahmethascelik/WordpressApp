3768e1f6bbe0484eb09d275e589fa410d81b8920
ahmethascelik <ahmethascelik@gmail.com>
2023-04-04 11:26:13 +0300

    Initial commit

---
+++ app/src/main/java/com/teb/wordpressapp/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.teb.wordpressapp.ui
+
+import android.app.Activity
+import android.os.Bundle
+import com.teb.wordpressapp.R
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+    }
+}
