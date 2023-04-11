86bb0bcc68e22a0a9d650556d60828702809f244
Ömer Oğuz <omeroguzcelikel@gmail.com>
2023-04-11 10:22:55 +0300

    Commets page can go back

--- app/src/main/java/com/teb/wordpressapp/ui/CommentActivity.kt
+++ app/src/main/java/com/teb/wordpressapp/ui/CommentActivity.kt
@@ -1,11 +1,38 @@
 package com.teb.wordpressapp.ui
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import com.teb.wordpressapp.R
+import com.teb.wordpressapp.databinding.ActivityCommentBinding
 
 class CommentActivity: BaseActivity() {
+
+    private lateinit var binding: ActivityCommentBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comment)
+        binding = ActivityCommentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews();
+    }
+
+    private fun initViews() {
+
+        defaultLoadingCallback = { isLoading ->
+            if (isLoading) {
+                binding.progressBar.visibility = View.VISIBLE
+            } else {
+                binding.progressBar.visibility = View.GONE
+            }
+        }
+        binding.toolbar.setTitleTextColor(Color.WHITE)
+
+        binding.toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+
     }
 }
