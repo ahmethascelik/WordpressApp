4c2ca81ad9e9974cf00f190edac034ae4c9f1325
ahmethascelik <ahmethascelik@gmail.com>
2023-04-19 17:09:07 +0300

    notification fix

--- wpcore/src/main/java/com/teb/wpcore/pn/PnService.kt
+++ wpcore/src/main/java/com/teb/wpcore/pn/PnService.kt
@@ -1,10 +1,10 @@
 package com.teb.wpcore.pn
 
-import android.R
-import android.app.Notification
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
+import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
 import android.util.Log
@@ -55,7 +55,7 @@ class PnService : FirebaseMessagingService() {
             url = message.data["url"],
         )
 
-        if(pushData.action.equals("open_url")){
+        if (pushData.action.equals("open_url")) {
 
             val notificationIntent = Intent(this, MainActivity::class.java)
 
@@ -63,32 +63,46 @@ class PnService : FirebaseMessagingService() {
             notificationIntent.data = Uri.parse(pushData.url)
 
 
-            val contentIntent: PendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                PendingIntent.getActivity(this,
-                    0,
-                    notificationIntent,
-                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
-            } else {
-                PendingIntent.getActivity(this,
-                    0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+            //  RxBus.publish(RxBus.SUBJECT_MY_SUBJECT,intent.getExtras());
+            var flags = PendingIntent.FLAG_UPDATE_CURRENT
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                flags = flags or PendingIntent.FLAG_IMMUTABLE
             }
 
-            val b = NotificationCompat.Builder(this)
-
-            b.setAutoCancel(true)
-                .setDefaults(Notification.DEFAULT_ALL)
-                .setWhen(System.currentTimeMillis())
-                .setSmallIcon(R.drawable.ic_delete)
-                .setContentText(pushData.text)
-                .setDefaults(Notification.DEFAULT_LIGHTS or Notification.DEFAULT_SOUND)
-//                .setContentIntent(contentIntent)
-                .setContentInfo("Info")
+            val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, flags)
 
 
-            val notificationManager =
-                this.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.notify(1, b.build())
-        }else if ( pushData.action.equals("change_logo")){
+            var soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+            val notificationBuilder: NotificationCompat.Builder =
+                NotificationCompat.Builder(this, "TEB")
+                    .setSmallIcon(com.teb.wpcore.R.drawable.ic_baseline_arrow_back_24)
+                    .setContentTitle(getString(com.onesignal.R.string.app_name))
+                    .setContentText(pushData.text)
+                    .setStyle(NotificationCompat.BigTextStyle().bigText(pushData.text))
+                    .setAutoCancel(true)
+                    .setSound(soundUri)
+                    .setContentIntent(pendingIntent)
+
+            var notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val name: CharSequence = "name"
+                val description = "CEPTETEB Notification"
+                val importance = NotificationManager.IMPORTANCE_DEFAULT
+                val channel = NotificationChannel("TEB", name, importance)
+                channel.description = description
+                // Register the channel with the system; you can't change the importance
+                // or other notification behaviors after this
+                notificationManager = getSystemService(NotificationManager::class.java)
+                notificationManager.createNotificationChannel(channel)
+            }
+
+            notificationManager.notify(1, notificationBuilder.build())
+
+
+        } else if (pushData.action.equals("change_logo")) {
             val persistance = ServiceLocator.providePersistance();
             pushData.url?.let { persistance.setCustomLogo(this, it) }
         }
